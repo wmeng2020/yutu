bdcc8ba5fea5703d9a06741e3822912e46a06141
leo <736750847@qq.com>
2020-11-30 14:46:00 +0800

    12313
    Signed-off-by: leo <736750847@qq.com>

--- application/index/validate/BindingOther.php
+++ application/index/validate/BindingOther.php
@@ -7,14 +7,14 @@ class BindingOther extends Validate
 {
     protected $rule = [
         'types' => 'require',
-        'image' => 'require',
+//        'image' => 'require',
         'account' => 'require',
         'mobile' => 'require',
     ];
 
     protected $message = [
         'types.require' => '类型不能为空',
-        'image.require' => '图片不能为空',
+//        'image.require' => '图片不能为空',
         'account.require' => '账号不能为空',
         'mobile.require' => '手机号不能为空',
 
